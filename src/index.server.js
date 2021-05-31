cbeeb47ef74b8ffe83a428c43e998caef4b3a13f
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-31 14:31:29 +0530

    First Commit

---
+++ src/index.server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const env = require('dotenv')
+
+// environment variable or you can say constants
+const app = express()
+env.config()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})

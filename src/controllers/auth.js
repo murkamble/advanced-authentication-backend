0182c3e7d4db53c046afe2a116e71fe3f2283c34
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-31 18:34:50 +0530

    middlerware error in auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const ErrorResponse = require('../utlis/errorResponse')
 
 exports.register = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -9,21 +10,18 @@ exports.register = async (req, res, next) => {
             user,
         })
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        })
+        next(error)
     }
 }
 
 exports.login = async (req, res, next) => {
     const { email, password } = req.body;
-    if (!email || !password) { res.status(400).json({ success: false, error: 'Please provide email and password' }) }
+    if (!email || !password) { return next(new ErrorResponse('Please provide email and password'), 400) }
     try {
         const user = await User.findOne({ email }).select('+password')
-        if (!user) { res.status(404).json({ success: false, error: 'Invalid Creadentials' }) }
+        if (!user) { return next(new ErrorResponse('Invalid Creadentials'), 404) }
         const isMatch = await user.matchPasswords(password)
-        if (!isMatch) { res.status(404).json({ success: false, error: 'Invalid Creadentials' }) }
+        if (!isMatch) { return next(new ErrorResponse('Invalid Creadentials'), 404) }
         res.status(200).json({ success: true, token: 'ad5as2df1' }) 
     } catch (error) {
         res.status(500).json({ success: false, error: error.message })

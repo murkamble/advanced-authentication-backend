17eb9ad1a6285dc5a5149a545c50697ef818354b
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-31 16:42:57 +0530

    Mongoose Connect

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,16 @@
-exports.register = (req, res, next) => {
-    res.send('Register route')
+const User = require('../models/User')
+
+exports.register = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    try {
+        const user = await User.create({ username, email, password })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
 }
 
 exports.login = (req, res, next) => {
--- src/index.server.js
+++ src/index.server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const env = require('dotenv')
+const connectDB = require('./config/db')
 
 // environment variable or you can say constants
 const app = express()
 env.config()
 app.use(express.json())
+
+// mongoose Databases connection string
+connectDB()
 
 // routes
 const authRoutes = require('./routes/auth')
@@ -12,6 +16,13 @@ const authRoutes = require('./routes/auth')
 // api
 app.use('/api', authRoutes)
 
+// Listing Port
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+// Process Error
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Logged Error: ${err}`)
+    server.close(() => process.exit(1))
 })

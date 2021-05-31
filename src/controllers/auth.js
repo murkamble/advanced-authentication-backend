9cb6bf2075c8422f5ea4b06109d5963c73f32a97
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-31 16:53:11 +0530

    auth register update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,7 +4,10 @@ exports.register = async (req, res, next) => {
     const { username, email, password } = req.body;
     try {
         const user = await User.create({ username, email, password })
-
+        res.status(201).json({
+            success: true,
+            user,
+        })
     } catch (error) {
         res.status(500).json({
             success: false,
